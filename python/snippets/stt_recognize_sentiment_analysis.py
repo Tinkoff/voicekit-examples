4f899abb582052bddffb4e813b8ccd43f13ef1f5
Nikolai Semenov <semenov.nn@phystech.edu>
2021-07-26 19:24:17 +0300

    fix: use Tinkoff AI API

--- python/snippets/stt_recognize_sentiment_analysis.py
+++ python/snippets/stt_recognize_sentiment_analysis.py
@@ -8,7 +8,7 @@ from auth import authorization_metadata
 import grpc
 import os
 
-endpoint = os.environ.get("VOICEKIT_ENDPOINT") or "stt.tinkoff.ru:443"
+endpoint = os.environ.get("VOICEKIT_ENDPOINT") or "api.tinkoff.ai:443"
 api_key = os.environ["VOICEKIT_API_KEY"]
 secret_key = os.environ["VOICEKIT_SECRET_KEY"]
 
