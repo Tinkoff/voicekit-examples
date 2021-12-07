8203531175cc1c1334cbdfc475e5b5d829b435d0
Nikolai Semenov <semenov.nn@phystech.edu>
2021-12-08 01:29:22 +0300

    feat: add ALAW example

---
+++ python/snippets/tts_streaming_synthesize_a_law_playback.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append("..")
+
+from tinkoff.cloud.tts.v1 import tts_pb2_grpc, tts_pb2
+from auth import authorization_metadata
+import grpc
+import os
+import pyaudio
+import audioop
+
+endpoint = os.environ.get("VOICEKIT_ENDPOINT") or "api.tinkoff.ai:443"
+api_key = os.environ["VOICEKIT_API_KEY"]
+secret_key = os.environ["VOICEKIT_SECRET_KEY"]
+
+sample_rate = 8000
+
+
+def build_request():
+    return tts_pb2.SynthesizeSpeechRequest(
+        input=tts_pb2.SynthesisInput(
+            text="Все ищут во мне тайну. А во мне нет тайны, во мне все просто и ясно. Никаких тайн. Я привык с жизнью "
+                 "встречаться прямо. Не отличая большого от малого."
+        ),
+        audio_config=tts_pb2.AudioConfig(
+            audio_encoding=tts_pb2.ALAW,
+            sample_rate_hertz=sample_rate,
+        ),
+    )
+
+
+pyaudio_lib = pyaudio.PyAudio()
+f = pyaudio_lib.open(output=True, channels=1, format=pyaudio.paInt16, rate=sample_rate)
+
+stub = tts_pb2_grpc.TextToSpeechStub(grpc.secure_channel(endpoint, grpc.ssl_channel_credentials()))
+request = build_request()
+metadata = authorization_metadata(api_key, secret_key, "tinkoff.cloud.tts")
+responses = stub.StreamingSynthesize(request, metadata=metadata)
+for key, value in responses.initial_metadata():
+    if key == "x-audio-duration-seconds":
+        print("Estimated audio duration is {:.2f} seconds".format(float(value)))
+        break
+for stream_response in responses:
+    f.write(audioop.alaw2lin(stream_response.audio_chunk, 2))
+f.stop_stream()
+f.close()
